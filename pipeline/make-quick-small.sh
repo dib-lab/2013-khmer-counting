c9ed5e99a6c62bcafa441b32bd4af4413bdf58f6
C. Titus Brown <titus@idyll.org>
2013-06-28 00:41:23 +0000

    script for making a subset of the data for testing

---
+++ pipeline/make-quick-small.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# make small files for running pipeline as a test
+
+if [ -d orig ]; then
+   echo 'orig dir already exists'
+   exit 0
+fi
+
+mkdir orig
+
+for filename in ecoli_ref.fastq iowa_prairie_0920.fa.1 iowa_prairie_0920.fa.2 iowa_prairie_0920.fa.3 iowa_prairie_0920.fa.4 iowa_prairie_0920.fa.5
+do
+   mv $filename orig/$filename && head -400000 orig/$filename > $filename
+done
