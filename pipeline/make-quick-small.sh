a5c3881082c1d04fb3d6d8e58e84db36fdc855a0
C. Titus Brown <titus@idyll.org>
2013-06-28 02:32:43 +0000

    updated to include missing file

--- pipeline/make-quick-small.sh
+++ pipeline/make-quick-small.sh
@@ -8,7 +8,7 @@ fi
 
 mkdir orig
 
-for filename in ecoli_ref.fastq iowa_prairie_0920.fa.1 iowa_prairie_0920.fa.2 iowa_prairie_0920.fa.3 iowa_prairie_0920.fa.4 iowa_prairie_0920.fa.5
+for filename in ecoli_ref.fastq iowa_prairie_0920.fa.1 iowa_prairie_0920.fa.2 iowa_prairie_0920.fa.3 iowa_prairie_0920.fa.4 iowa_prairie_0920.fa.5 MH0001.trimmed.fa
 do
    mv $filename orig/$filename && head -400000 orig/$filename > $filename
 done
